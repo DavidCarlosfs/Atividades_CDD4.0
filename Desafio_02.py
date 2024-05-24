8b1e07525b9a4dd87140b6e6389a6a5da4b8eb1c
David Carlos <davidcarlosfs@hotmail.com>
2024-05-23 22:20:42 -0300

    Desafio_02
    Atividade onde devemos criar um jogo de "pedra, papel e tesoura" entre 2 jogadores.

---
+++ Desafio_02.py
@@ -0,0 +1,54 @@
+#Criar um jogo de pedra, papel e tesoura
+jokenpo = ['Pedra', 'Papel', 'Tesoura']
+cont1 = 0
+cont2 = 0
+empate = 0
+
+for x in range(3):
+    player1 = int(input("Digite: \n"
+                    "0 para Pedra\n"
+                    "1 para Papel\n"
+                    "2 para Tesoura: "))
+
+    player2 = int(input("Digite: \n"
+                    "0 para Pedra\n"
+                    "1 para Papel\n"
+                    "2 para Tesoura: "))
+
+    if player1 == 0:
+        if player2 ==0:
+            empate+=1
+            print("Empatou!")
+        if player2 == 1:
+            cont2+=1
+            print("Jogador 2 venceu essa rodada!")
+        if player2 == 2:
+            cont1+=1
+            print("Jogador 1 venceu essa rodada!")
+    elif player1 == 1:
+        if player2 ==0:
+            cont1+=1
+            print("Jogador 1 venceu essa rodada!")
+        if player2 == 1:
+            empate+=1
+            print("Empatou!")
+        if player2 == 2:
+            cont2+=1
+            print("Jogador 2 venceu essa rodada!")
+    elif player1 == 2:
+        if player2 ==0:
+            cont2+=1
+            print("Jogador 2 venceu essa rodada!")
+        if player2 == 1:
+            cont1+=1
+            print("Jogador 1 venceu essa rodada!")
+        if player2 == 2:
+            empate+=1
+            print("Empatou!")
+
+if cont1 >= 2:
+    print("Jogador 1 venceu o jogo!")
+elif cont2 >= 2:
+    print("Jogador 2 venceu o jogo!")
+else:
+    print("O jogo terminou em empate!")
